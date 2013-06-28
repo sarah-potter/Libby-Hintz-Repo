4415abea7641fab8d7fe76f430a84c62ce495a86
Sarah Potter <sarah.potter@me.com>
2013-06-28 12:27:39 -0700

    edits to single blog post

--- wp-content/themes/libby/functions.php
+++ wp-content/themes/libby/functions.php
@@ -8,3 +8,16 @@ function woo_custom_cart_button_text() {
  
 }
 
+
+
+function search_with_button_submit() {
+?>
+<div id="searchform">
+	<form method="get" class="search_form_visible" action="<?php bloginfo('home'); ?>/">
+	<p><input class="text_input" type="text" value="Enter Text &amp; Click to Search" name="s" id="s" onfocus="if (this.value == 'Enter Text &amp; Click to Search') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Enter Text &amp; Click to Search';}" />
+	<input type="submit" class="my-search" id="searchsubmit" value="SEARCH" /></p>
+	</form>
+	</div>
+<?php
+}
+add_action('search_form', 'search_form');
--- wp-content/themes/libby/single.php
+++ wp-content/themes/libby/single.php
@@ -6,7 +6,7 @@
 
 get_header(); ?>
 
-<div id="content-container" class="full-width">
+<div id="content-container">
 	<div id="content" role="main">
 
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post();
@@ -27,11 +27,14 @@ get_header(); ?>
 				<a class="entry-format" href="<?php echo esc_url( get_post_format_link( get_post_format() ) ); ?>" title="<?php echo esc_attr( sprintf( __( 'All %s posts', 'coraline' ), get_post_format_string( get_post_format() ) ) ); ?>"><?php echo get_post_format_string( get_post_format() ); ?></a>
 			<?php endif; ?>
 
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			<?php the_title( '<h1 class="blog-entry-title">', '</h1>' ); ?>
 
 			<?php if ( 'standard' == $format ) : ?>
 				<div class="entry-meta">
-					<?php coraline_posted_on(); coraline_posted_by(); ?><span class="comments-link"><span class="meta-sep">|</span> <?php comments_popup_link( __( 'Leave a comment', 'coraline' ), __( '1 Comment', 'coraline' ), __( '% Comments', 'coraline' ) ); ?></span>
+					<?php coraline_posted_on(); coraline_posted_by(); ?><span class="comments-link"><span class="meta-sep">|</span> 
+					
+					<?php comments_popup_link( __( 'Leave a comment', 'coraline' ), __( '1 Comment', 'coraline' ), __( '% Comments', 'coraline' ) ); ?></span>
+					
 					<?php edit_post_link( __( 'Edit', 'coraline' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
 				</div><!-- .entry-meta -->
 			<?php endif; ?>
@@ -41,16 +44,7 @@ get_header(); ?>
 				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'coraline' ), 'after' => '</div>' ) ); ?>
 			</div><!-- .entry-content -->
 
-			<div class="entry-info">
-			<?php if ( 'standard' != $format ) : ?>
-				<p class="format-entry-meta">
-					<?php coraline_posted_on(); coraline_posted_by(); ?>
-					<span class="comments-link"><?php comments_popup_link( __( '&rarr; Leave a comment', 'coraline' ), __( '&rarr; 1 Comment', 'coraline' ), __( '&rarr; % Comments', 'coraline' ) ); ?></span>
-				</p>
-			<?php endif; ?>
-				<?php coraline_posted_in(); ?>
-				<?php edit_post_link( __( 'Edit', 'coraline' ), '<span class="edit-link">', '</span>' ); ?>
-			</div><!-- .entry-info -->
+		
 		</div><!-- #post-## -->
 
 		<div id="nav-below" class="navigation">
